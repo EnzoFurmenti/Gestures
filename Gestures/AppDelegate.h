4ca57d84a4e522a7d3ab7677f26abf9ce06a0bf6
EnzoFurmenti <ducheenzo@mail.ru>
2016-09-01 17:00:47 +0300

    Initial Commit

---
+++ Gestures/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Gestures
+//
+//  Created by EnzoF on 01.09.16.
+//  Copyright © 2016 EnzoF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Gestures/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Gestures
+//
+//  Created by EnzoF on 01.09.16.
+//  Copyright © 2016 EnzoF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
