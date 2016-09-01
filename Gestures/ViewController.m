d9d7fc0f05c01eab6bb7924dae2d5965720e94c4
EnzoFurmenti <ducheenzo@mail.ru>
2016-09-01 22:19:25 +0300

    level student completed

--- Gestures/ViewController.m
+++ Gestures/ViewController.m
@@ -16,6 +16,20 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    CGRect rect = CGRectMake(CGRectGetMidX(self.view.bounds), CGRectGetMidY(self.view.bounds), CGRectGetWidth(self.view.bounds) /2 , CGRectGetHeight(self.view.bounds) /2);
+    UIImageView *imageView = [[UIImageView alloc] initWithFrame:rect];
+    [self.view addSubview:imageView];
+    imageView.backgroundColor = [UIColor greenColor];
+    imageView.center = self.view.center;
+    UIImage *imageStep1 = [UIImage imageNamed:@"step-1.png"];
+    UIImage *imageStep2 = [UIImage imageNamed:@"step-2.png"];
+    UIImage *imageStep3 = [UIImage imageNamed:@"step-3.png"];
+    UIImage *imageStep4 = [UIImage imageNamed:@"step-4.png"];
+    UIImage *imageStep5 = [UIImage imageNamed:@"step-5.png"];
+    NSArray *arrayImage = [[NSArray alloc] initWithObjects:imageStep1,imageStep2,imageStep3,imageStep4,imageStep5, nil];
+    imageView.animationImages = arrayImage;
+    imageView.animationDuration = 2.f;
+    [imageView startAnimating];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
